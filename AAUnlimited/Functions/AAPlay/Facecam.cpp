408abdce75dfa2988c48fdc641711046209e8815
aa2gcoder <unforgiven.II@mail.ru>
2016-10-07 03:20:49 -0400

    Tweaked POV mode. Q toggles POV, W toggles head visibility

--- AAUnlimited/Functions/AAPlay/Facecam.cpp
+++ AAUnlimited/Functions/AAPlay/Facecam.cpp
@@ -7,6 +7,7 @@
 #include "External\ExternalClasses\HClasses\HInfo.h"
 #include "External\ExternalClasses\Bone.h"
 #include "General\ModuleInfo.h"
+#include "Files\Logger.h"
 
 /*
  * Here is the glorious plan (please dont laugh):
@@ -27,22 +28,52 @@ ExtClass::HInfo* loc_hinfo = NULL;
 ExtClass::Bone* loc_focusBone = NULL;
 int loc_state = 0;
 
+BYTE m_activeFaceSlot = BYTE(255);
+BYTE m_passiveFaceSlot = BYTE(255);
+
+ExtClass::CharacterData::Hair* m_activeHair = NULL;
+ExtClass::CharacterData::Hair* m_passiveHair = NULL;
+
 void PostTick(ExtClass::HInfo* hInfo, bool notEnd) {
 	if (g_Config.GetKeyValue(Config::USE_H_FACECAM).bVal == false) return;
 	if (!notEnd) {
 		loc_state = 0;
 		loc_focusBone = NULL;
 		loc_hinfo = NULL;
+		if (m_passiveHair != NULL) {
+			delete m_passiveHair;
+			m_passiveHair == NULL;
+		}
+		if (m_activeHair != NULL) {
+			delete m_activeHair;
+			m_activeHair == NULL;
+		}
+		m_activeFaceSlot = BYTE(255);
+		m_passiveFaceSlot = BYTE(255);
 	}
 	else {
+		const BYTE faceMaleFaceless = 90;
+		ExtClass::CharacterData::Hair baldHaircut = ExtClass::CharacterData::Hair();
+			baldHaircut.frontHair = 0;
+			baldHaircut.sideHair = 0;
+			baldHaircut.backhair = 0;
+			baldHaircut.hairExtension = 0;
+		
 		loc_hinfo = hInfo;
+		//remember faces the first time
+		if (m_passiveFaceSlot == BYTE(255)) m_passiveFaceSlot = loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_faceSlot;
+		if (m_activeFaceSlot == BYTE(255)) m_activeFaceSlot = loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_faceSlot;
+		//remember hairstyles the first time
+		if (m_passiveHair == NULL) m_passiveHair = new ExtClass::CharacterData::Hair(loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_hair);
+		if (m_activeHair == NULL) m_activeHair = new ExtClass::CharacterData::Hair(loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_hair);
+
 		if (loc_state != 0) {
 
 			//we need to keep all camera parameters at 0 (except for fov of course)
 			ExtClass::HCamera* cam = hInfo->GetCamera();
 
-			cam->m_xRotRad = 0;
-			cam->m_yRotRad = M_PI;
+			//cam->m_xRotRad = 0;
+			//cam->m_yRotRad = M_PI;
 			cam->m_zRotRad = 0;
 
 			cam->m_distToMid = 0;
@@ -52,7 +83,9 @@ void PostTick(ExtClass::HInfo* hInfo, bool notEnd) {
 			cam->m_zShift = 0;
 
 			//we align the camera with the bone by copying the matrix
-			cam->m_matrix = loc_focusBone->m_matrix2;
+			auto mat = loc_focusBone->m_matrix2;
+			mat._42 += 1;
+			cam->m_matrix = mat;
 
 			//*(BYTE*)(General::GameBase + 0x3A6C80) = 3; //whether the q button is pressed
 		}
@@ -63,13 +96,59 @@ void PostTick(ExtClass::HInfo* hInfo, bool notEnd) {
 
 
 void AdjustCamera(ExtClass::Bone* bone) {
+	const BYTE faceMaleFaceless = 90;
+	ExtClass::CharacterData::Hair baldHaircut = ExtClass::CharacterData::Hair();
+		baldHaircut.frontHair = 0;
+		baldHaircut.sideHair = 0;
+		baldHaircut.backhair = 0;
+		baldHaircut.hairExtension = 0;
+
+
+	LOGPRIO(Logger::Priority::INFO) << bone->m_name << "\n";
 	if (loc_hinfo == NULL) return;
 	//make sure the q button was pressed, which means bone is the first bone of participant 2 (or 1 depending on position ._.)
-	if (bone != loc_hinfo->m_passiveParticipant->m_charPtr->m_bonePtrArray[0] 
-		&& bone != loc_hinfo->m_activeParticipant->m_charPtr->m_bonePtrArray[0]) return;
+	//	Q - toggles states
+	//	W - toggles head visibility depending on the state:
+	//		state 1 - passive's POV
+	//		state 2 - active's POV
+	//		state 3 - default camera
+	if (bone != loc_hinfo->m_passiveParticipant->m_charPtr->m_bonePtrArray[0]
+		&& bone != loc_hinfo->m_activeParticipant->m_charPtr->m_bonePtrArray[0]) {	//if not Q was pressed
 
-	//switch state
-	loc_state++;
+		if (bone == loc_hinfo->m_passiveParticipant->m_charPtr->m_bonePtrArray[1]
+			|| bone == loc_hinfo->m_activeParticipant->m_charPtr->m_bonePtrArray[1]) { //if W was pressed
+			
+			if (loc_state == 1) { //toggle passive
+				if (loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_faceSlot != faceMaleFaceless) {			//if head is visible
+					loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_faceSlot = faceMaleFaceless;				//set passive's face slot to faceless(male only for now)
+					loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_hair = baldHaircut;						//set passive's haircut to bald
+				} else {
+					loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_faceSlot = m_passiveFaceSlot;	//restore passive's face slot
+					loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_hair = ExtClass::CharacterData::Hair(*m_passiveHair);			//restore passive's haircut
+				}
+			} else if (loc_state == 2) { //toggle active
+				if (loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_faceSlot != faceMaleFaceless) {			//if head is visible
+					loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_faceSlot = faceMaleFaceless;				//set active's face slot to faceless(male only for now)
+					loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_hair = baldHaircut;						//set active's haircut to bald
+				} else {
+					loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_faceSlot = m_activeFaceSlot;				//restore active's face slot
+					loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_hair = ExtClass::CharacterData::Hair(*m_activeHair);			//restore active's haircut
+				}
+			} else { //restore both
+				loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_faceSlot = m_activeFaceSlot;				//restore active's face slot
+				loc_hinfo->m_activeParticipant->m_charPtr->m_charData->m_hair = ExtClass::CharacterData::Hair(*m_activeHair);			//restore active's haircut
+
+				loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_faceSlot = m_passiveFaceSlot;				//restore passive's face slot
+				loc_hinfo->m_passiveParticipant->m_charPtr->m_charData->m_hair = ExtClass::CharacterData::Hair(*m_passiveHair);			//restore passive's haircut
+			}
+			return;
+		}
+		else return;
+	} else { //if Q was pressed
+		//switch state
+		loc_state++;
+	}
+
 	if (loc_state == 3) {
 		//if we come back to free camera mode, restore the matrix to an identity matrix
 		loc_state = 0;
@@ -80,6 +159,7 @@ void AdjustCamera(ExtClass::Bone* bone) {
 											0,0,0,1.0f
 										};
 		loc_hinfo->GetCamera()->m_matrix = idMatr;
+
 	}
 
 	//set bone depending on state
