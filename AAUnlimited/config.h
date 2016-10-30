dbad1e6f7f4752cbedce1604f7cc15a9836edb75
linkanon <unforgiven.II@mail.ru>
2016-10-30 04:24:43 -0400

    changed override root paths

--- AAUnlimited/config.h
+++ AAUnlimited/config.h
@@ -5,7 +5,7 @@
 
 #define CONFIG_FILE_PATH TEXT("AAUnlimited\\config.txt")
 #define LOGGER_FILE_PATH TEXT("AAUnlimited\\logfile.txt")
-#define OVERRIDE_IMAGE_PATH TEXT("data\\texture\\override\\")
-#define OVERRIDE_ARCHIVE_PATH TEXT("data\\")
-#define TAN_PATH TEXT("data\\texture\\override\\tan\\")
-#define HAIR_HIGHLIGHT_PATH TEXT("data\\texture\\override\\hair_highlight\\")
+#define OVERRIDE_IMAGE_PATH TEXT("data\\override\\")
+#define OVERRIDE_ARCHIVE_PATH TEXT("data\\override\\")
+#define TAN_PATH TEXT("data\\override\\tan\\")
+#define HAIR_HIGHLIGHT_PATH TEXT("data\\override\\hair_highlights\\")
