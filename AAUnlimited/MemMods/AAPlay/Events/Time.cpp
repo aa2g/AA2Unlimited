cca35a52d433952aca0031a7a67010138c85cd6b
Genei Shouko <anonymous@4chan.org>
2017-04-28 10:09:51 -0500

    fix period change injection crash

--- AAUnlimited/MemMods/AAPlay/Events/Time.cpp
+++ AAUnlimited/MemMods/AAPlay/Events/Time.cpp
@@ -34,14 +34,15 @@ void __declspec(naked) PeriodChangeRedirect() {
 		mov eax,[eax+20]
 		push eax //save old period
 
-		push [esp+4]
+		push [esp+8] //push original function argument
 		call [loc_PeriodChangeOriginalFunction]
 		push eax //save return value for later
 
 		push eax
 		call PeriodChangeEvent
 
-		pop eax
+		pop eax //set return value
+		add esp,4 //remove old period from stack
 		ret 4
 	}
 }
