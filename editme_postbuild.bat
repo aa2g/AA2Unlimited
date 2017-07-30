25dbce3bd591926f8711df5db0efe71c1e7c131e
ez <ezzzdiy@gmail.com>
2017-07-30 05:58:10 +0200

    Rename aau.dll back to $(projectname).

--- editme_postbuild.bat
+++ editme_postbuild.bat
@@ -1,5 +1,5 @@
 set DST=c:\mega\aa2mini\aau
-copy /Y %1\aau.dll %DST% > nul || exit /b 1 
+copy /Y %1\AAUnlimitedDLL.dll %DST%\aau.dll > nul || exit /b 1 
 cd AAUnlimited\Texts || exit /b 1
 copy /Y *.lua %DST%\  > nul || exit /b 1
 xcopy /Y /E mod %DST%\mod > nul  || exit /b 1
