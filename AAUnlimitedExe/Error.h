4d935d2060e1815c7bdb5790a272431afe17f4f7
ez <ezzzdiy@gmail.com>
2017-08-07 04:20:07 +0200

    Nuke AAUnlimitedExe

--- AAUnlimitedExe/Error.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-namespace Error {
-	void PrintLastError(const char* prefix = NULL, const char* postfix = NULL);
-	void SetLastError(int error,const char* context);
-}
