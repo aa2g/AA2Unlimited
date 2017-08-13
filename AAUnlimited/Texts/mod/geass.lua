37311fbeb988b52504f8fd7ffbe874620b441ad2
Genei Shouko <anonymous@4chan.org>
2017-08-12 21:10:21 -0500

    configure geass

--- AAUnlimited/Texts/mod/geass.lua
+++ AAUnlimited/Texts/mod/geass.lua
@@ -2,10 +2,13 @@
 
 local _M = {}
 
+local keys = { 0x01, 0x02, 0x04, 0x10, 0x11, 0x12 }
+local opts
+
 function on.answer(resp, answering, asking)
 	if asking.m_thisChar == GetPlayerCharacter() then
-		-- right mouse button is down
-		if (GetAsyncKeyState(2) & 0x8000) ~= 0 then
+		-- activation key is down
+		if (GetAsyncKeyState(keys[opts.key + 1]) & 0x8000) ~= 0 then
 			print("GEASS: forcing NPC answer to yes")
 			return true
 		end
@@ -13,10 +16,27 @@ function on.answer(resp, answering, asking)
 	return resp
 end
 
-function _M.load()
+function _M:load()
+	opts = self
+	self.key = self.key or 2
 end
 
-function _M.unload()
+function _M:unload()
+end
+
+function _M:config()
+	if not self then return end
+	require "iuplua"
+	require "iupluacontrols"
+
+	local ok, newkey = iup.GetParam("Set the activation key for Geass", nil, [[
+Activation Key: %l|Left Mouse Button|Right Mouse Button|Middle Mouse Button|Shift Key|Control Key|Alt Key|
+]], self.key)
+	if ok then
+		self.key = newkey
+	end
+
+	Config.save()
 end
 
 return _M
