57e829771e00fb17b03e6dfd36d76054324915c5
ez <ezzzdiy@gmail.com>
2017-08-16 02:24:13 +0200

    ui_event has one more argument in call signature

--- AAUnlimited/MemMods/AAPlay/Events/UiEvent.cpp
+++ AAUnlimited/MemMods/AAPlay/Events/UiEvent.cpp
@@ -5,7 +5,7 @@ namespace UIEvent {
 
 DWORD orig_uievent;
 
-bool __declspec(naked) __cdecl OrigUiEvent(int evt, BYTE par1, BYTE par2, bool par3, int time1, int time2)
+int __declspec(naked) __cdecl OrigUiEvent(int evt, int par1, int par2, int par3, int time1, int time2, int tim3)
 {
 	__asm {
 		push ebp
@@ -22,9 +22,9 @@ bool __declspec(naked) __cdecl OrigUiEvent(int evt, BYTE par1, BYTE par2, bool p
 // par1 and par2 are usually between 0..2
 // par3 unknown
 // time1 and time2 are time values in ms, probably speed of fadeout
-bool __cdecl UiEventHook(int state, BYTE par1, BYTE par2, bool par3, int time1, int time2) {
-	LUA_EVENT_NORET("ui_event", state,par1,par2,par3,time1,time2)
-	return OrigUiEvent(state, par1, par2, par3, time1, time2);
+int __cdecl UiEventHook(int state, int par1, int par2, int par3, int time1, int time2, int time3) {
+	LUA_EVENT_NORET("ui_event", state,par1,par2,par3,time1,time2, time3)
+	return OrigUiEvent(state, par1, par2, par3, time1, time2, time3);
 }
 
 void Inject() {
@@ -33,7 +33,6 @@ void Inject() {
 	{ 0x55, 0x8b, 0xec, 0x83, 0xe4, 0xf8 },                                                               //expected values
 	{ 0x90, 0xE9, HookControl::RELATIVE_DWORD, (DWORD)&UiEventHook}, //redirect to our function
 		NULL);
-
 }
 }
 }
