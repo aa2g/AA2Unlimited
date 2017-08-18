f358a0c7c777d773d0ee66c8614be1b9d75ad779
ez <ezzzdiy@gmail.com>
2017-08-18 14:39:16 +0200

    Avoid raising certain edit sliders to 255
    ie those which are still stuck at 100 anyway

--- AAUnlimited/Texts/mod/unlocks.lua
+++ AAUnlimited/Texts/mod/unlocks.lua
@@ -225,18 +225,30 @@ end
 
 function _M:load()
 	mod_load_config(self, options)
+	local avoid = {
+		[10176] = true,
+		[10292] = true,
+		[10356] = true,
+		[10440] = true,
+		[10551] = true,
+		[10355] = true,
+	}
 
 	if exe_type == "edit" then
 		send_msg = g_hook_vptr(0x002C43E0, 4, function(orig, this, hdlg, msg, wparam, lparam)
 			-- slider range - something asking for 100, make it 255
-			if msg == 0x408 and wparam == 1 and lparam == 100 then
-				--log("OVERRIDE 100->255 %x %x %x %x %x %x", orig,this,hdlg,msg,wparam,lparam)
-				lparam = 255 
-			end
+			local itemid = GetWindowLongW(hdlg, -12)
+			if not avoid[itemid] then
 
-			-- text field size, 6 and 26 -> 255
-			if msg == 0xc5 and (wparam == 26) or (wparam == 6) then
-				wparam = 255
+				if msg == 0x408 and wparam == 1 and lparam == 100 then
+					--log("OVERRIDE 100->255 %x %x %x %x %x %x", orig,this,hdlg,msg,wparam,lparam)
+					lparam = 255 
+				end
+
+				-- text field size, 6 and 26 -> 255
+				if msg == 0xc5 and (wparam == 26) or (wparam == 6) then
+					wparam = 255
+				end
 			end
 
 
