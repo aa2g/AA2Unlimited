6910ea4f401b570ec2f8b9a005caec336d0a3fd7
ez <ezzzdiy@gmail.com>
2017-09-17 03:35:57 +0200

    fix broken fullscreen

--- AAUnlimited/Texts/mod/poser/dlg.lua
+++ AAUnlimited/Texts/mod/poser/dlg.lua
@@ -670,7 +670,7 @@ signals.connect(dialogposes, "savescene", _M, "savescene")
 -- https://www.codeproject.com/Articles/11114/Move-window-form-without-Titlebar-in-C
 -- Of course iup supports nothing of the sorts, so we have to do it the dirty way
 local function adjust_parenting(v)
-	if (not _M.forceparenting) or (opts.ontop == 0) then return end
+	if (not _M.forceparenting) or (_M.opts.ontop == 0) then return end
 	v.menubox = "no"
 	v:map()
 	set_window_proc(v.hwnd, function(orig, this, hwnd, msg, wparam, lparam)
@@ -705,7 +705,7 @@ function _M.updatefloating(d)
 	d = d or dialogs
 	if not d then return end
 	local parent = 0
-	if (_M.forceparenting and opts.ontop == 1) then parent = _M.parentHWND end
+	if (_M.forceparenting and _M.opts.ontop == 1) then parent = _M.parentHWND end
 	for _,v in ipairs(d) do
 		SetParent(v.hwnd, parent)
 	end
