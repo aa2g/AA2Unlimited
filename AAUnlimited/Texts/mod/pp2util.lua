fe7a83ee9de6b778b7082363290aa04936af4632
ez <ezzzdiy@gmail.com>
2017-09-30 20:23:08 +0200

    add pp2 extractor utility

---
+++ AAUnlimited/Texts/mod/pp2util.lua
@@ -0,0 +1,48 @@
+pp2matched = {}
+function pp2match(n,limit)
+	limit = limit or 20
+	local matched = {}
+	local nmatched = 0
+	pp2matched = matched
+	n = n:lower()
+	print('matching ' .. n)
+	for pp2idx,pp2name in ipairs {PP2List()} do
+		--print(pp2name)
+		for fname,fidx in pairs(PP2GetNames(pp2idx-1)) do
+			--print(pp2name,fname,fidx)
+			--if true then return end
+			if fname:lower():match(n) then
+				local ppname, fname2 = fname:match("^(.+)%.pp/([^/]*)$")
+				if ppname and (not matched[fname]) then
+					matched[fname] = {pp2idx-1,fidx,ppname,fname2}
+					if nmatched < limit then
+						print("Found %s:\t%s/%s" % {pp2name, ppname, fname2})
+					end
+					nmatched = nmatched+1
+				end
+			end
+		end
+	end
+	if nmatched >= limit then
+		print("... and %d more files not shown" % (nmatched-limit))
+	end
+	print("Found %d total. Type pp2dump() to extract those." % {nmatched})
+end
+
+function pp2dump(n)
+	local dpath = play_path('data')
+	print("Dumping files into %s" % {dpath})
+	for _,v in pairs(pp2matched) do
+		local pp2idx, fidx, ppname, fname = table.unpack(v)
+		local data = PP2ReadFile(pp2idx, fidx)
+		CreateDirectoryA(play_path('data',ppname), 0)
+		local tgt = play_path('data',ppname,fname)
+		if tgt:match("%.wav$") then
+			tgt = tgt .. ".opus"
+		end
+		print("Dumping %s" % tgt)
+		local fo = io.open(tgt, "wb")
+		fo:write(data)
+		fo:close()
+	end
+end
