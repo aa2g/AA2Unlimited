8709cb7e447b99f7ad5909ff3d751ab31c5b320b
ez <ezzzdiy@gmail.com>
2017-10-04 05:42:27 +0200

    set high compression settings on pp2

--- pp2/pp2.cs
+++ pp2/pp2.cs
@@ -40,7 +40,7 @@ public class DirEntry : IReadFile
 // This can be easily interfac-ied, needs to give only .GetData(), .GetMeta() & .naked
 public class ChunkFile
 {
-    const int MAXSIZE = 16 * 1024 * 1024;
+    const int MAXSIZE = 32 * 1024 * 1024;
     // helper fields
     public bool done, naked; // no zstd, and no chunk grouping
     public IReadFile pp;
@@ -374,7 +374,7 @@ public class ChunkBuilder
         int clen;
         fixed (byte* pcbuf = cbuf)
         {
-            clen = (int)ZSTD.ZSTD_compress(new IntPtr(pcbuf), (IntPtr)cbuf.Length, input, (IntPtr)input.Length, 3);
+            clen = (int)ZSTD.ZSTD_compress(new IntPtr(pcbuf), (IntPtr)cbuf.Length, input, (IntPtr)input.Length, 22);
         }
         var res = new MemoryStream();
         res.Write(cbuf, 0, clen);
