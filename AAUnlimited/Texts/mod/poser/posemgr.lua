e8567dd9ac2f4371ea30515b3b8ccd49c85cfb18
Genei Shouko <anonymous@4chan.org>
2017-10-03 20:54:47 -0500

    update blush load and save

--- AAUnlimited/Texts/mod/poser/posemgr.lua
+++ AAUnlimited/Texts/mod/poser/posemgr.lua
@@ -147,8 +147,15 @@ local function table2pose(pose, character)
 		if face.eye then character.eye = face.eye end
 		if face.eyeopen then character.eyeopen = face.eyeopen end
 		if face.eyebrow then character.eyebrow = face.eyebrow end
-		if face.blush then character.blush = face.blush / 9 end
-		if face.blushlines then character.blushlines = face.blushlines / 9 end
+
+		local material = charamgr.current.struct.m_xxFace:FindMaterial("A00_M_hoho") or face:FindMaterial("S00_M_hoho") 
+		if material then
+			material:m_lightingAttributes(3, face.blush / 9)
+		end
+		material = charamgr.current.struct.m_xxFace:FindMaterial("A00_M_hohosen") or face:FindMaterial("S00_M_hohosen") 
+		if material then
+			material:m_lightingAttributes(3, face.blush / 9)
+		end
 	end
 end
 
@@ -211,9 +218,17 @@ local function pose2table(character)
 			eyebrow = character.eyebrow,
 			mouth = character.mouth,
 			mouthopen = character.mouthopen,
-			blush = character.blush * 9,
-			blushlines = character.blushlines * 9,
 		}
+
+		local material = charamgr.current.struct.m_xxFace:FindMaterial("A00_M_hoho") or face:FindMaterial("S00_M_hoho") 
+		if material then
+			face.blush = material:m_lightingAttributes(3) * 9
+		end
+		material = charamgr.current.struct.m_xxFace:FindMaterial("A00_M_hohosen") or face:FindMaterial("S00_M_hohosen") 
+		if material then
+			face.blushlines = material:m_lightingAttributes(3) * 9
+		end
+
 		t.face = face
 		return t
 	end
