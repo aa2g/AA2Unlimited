be16f33da79b9946212e2dae8050b7a583b71b86
Genei Shouko <anonymous@4chan.org>
2017-10-13 17:23:37 -0500

    add props to scenes

--- AAUnlimited/Texts/mod/poser/posemgr.lua
+++ AAUnlimited/Texts/mod/poser/posemgr.lua
@@ -7,6 +7,7 @@ local signals = require "poser.signals"
 local json = require "json"
 local lists = require "poser.lists"
 local charamgr = require "poser.charamgr"
+local propmgr = require "poser.propmgr"
 local toggles = require "poser.toggles"
 
 local clipchanged= signals.signal()
@@ -267,6 +268,7 @@ function saveposebutton.action()
 	savepose(poselist.value)
 end
 
+
 -- == Scenes ==
 
 local function loadscene(filename)
@@ -280,11 +282,48 @@ local function loadscene(filename)
 			error("Error decoding scene %s data:" % filename)
 		end
 		if scene then
-			for i,loadchara in ipairs(scene.characters or {}) do
+			local characters = scene.characters or {}
+			local props = scene.props or {}
+			
+			local loadedprops = {}
+			for i,v in pairs(propmgr.props) do
+				loadedprops[i] = v
+			end
+			
+			for i,readchara in ipairs(characters) do
 				local chara = charamgr.characters[i]
 				if chara then
 					log.spam("table2pose")
-					table2pose(loadchara.pose, chara)
+					table2pose(readchara.pose, chara)
+				end
+			end
+			
+			for i,readprop in ipairs(props) do
+				local find
+				for j,p in pairs(loadedprops) do
+					if p.name == readprop.name then
+						find = j
+						break
+					end
+				end
+				find = table.remove(loadedprops, find)
+				if find then
+					for k,v in pairs(readprop.sliders) do
+						local slider = find:getslider(k)
+						if slider then
+							slider:rotation(0,v[1])
+							slider:rotation(1,v[2])
+							slider:rotation(2,v[3])
+							slider:rotation(3,v[4])
+							slider:translate(0,v[5])
+							slider:translate(1,v[6])
+							slider:translate(2,v[7])
+							slider:scale(0,v[8])
+							slider:scale(1,v[9])
+							slider:scale(2,v[10])
+							slider:Apply()
+						end
+					end
 				end
 			end
 		end
@@ -304,17 +343,44 @@ local function savescene(filename)
 	autopose(filename)
 	local path = aau_path(scenesdir, filename) .. ".scene"
 	log.spam("Poser: Saving scene %s to %s", filename, path)
+
+	local characters = {}
+	local props = {}
+
 	local scene = {
 		VERSION = 1,
-		characters = {},
+		characters = characters,
+		props = props,
 	}
-	local characters = scene.characters
 	
 	for _,chara in ipairs(charamgr.characters) do
 		local character = {
 			pose = pose2table(chara)
 		}
 		table.insert(characters, character)
+	end
+	
+	for _,prop in ipairs(propmgr.props) do
+		local sliders = {}
+		for k,v in prop:sliders() do
+			sliders[k] = {
+				v:rotation(0),
+				v:rotation(1),
+				v:rotation(2),
+				v:rotation(3),
+				v:translate(0),
+				v:translate(1),
+				v:translate(2),
+				v:scale(0),
+				v:scale(1),
+				v:scale(2),
+			}
+		end
+		local prop = {
+			name = prop.name,
+			sliders = sliders,
+		}
+		table.insert(props, prop)
 	end
 	
 	local file = io.open(path, "w")
