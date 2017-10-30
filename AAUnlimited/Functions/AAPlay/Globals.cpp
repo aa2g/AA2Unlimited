9eb41d49c1315df81baac36604caf2af77013733
linkanon <unforgiven.II@mail.ru>
2017-10-30 15:38:08 +0200

    Styles: m_currCardStyle is now stored into the save.json and read from on class load

--- AAUnlimited/Functions/AAPlay/Globals.cpp
+++ AAUnlimited/Functions/AAPlay/Globals.cpp
@@ -1,4 +1,5 @@
 #include "StdAfx.h"
+#include "Files\PersistentStorage.h"
 
 using namespace Shared::Triggers;
 
@@ -43,6 +44,12 @@ void InitOnLoad() {
 		//throw init event
 		CardInitializeData data;
 		data.card = seat;
+
+		auto inst = &AAPlay::g_characters[seat];
+		auto storage = PersistentStorage::ClassStorage::getStorage(Shared::GameState::getCurrentClassSaveName());
+		if (storage.getCardInt(inst, L"m_currCardStyle").isValid)
+			inst->m_cardData.m_currCardStyle = storage.getCardInt(inst, L"m_currCardStyle").value;
+
 		ThrowEvent(&data);
 
 		// ditto for lua
