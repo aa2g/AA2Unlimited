a2f9ab763f5cf79f030e42172b0023e8257570bf
Genei Shouko <date.aa2@gmail.com>
2018-06-26 09:35:29 -0500

    tweak postbuild.bat
    use original name
    install pdb file

--- editme_postbuild.bat
+++ editme_postbuild.bat
@@ -1,5 +1,6 @@
 set DST=c:\mega\aa2mini\aau
-copy /Y %1\AAUnlimitedDLL.dll %DST%\aau.dll > nul || exit /b 1 
+copy /Y %1\AAUnlimitedDLL.dll %DST%\\AAUnlimitedDLL.dll > nul || exit /b 1
+copy /Y %1\AAUnlimitedDLL.pdb %DST%\\AAUnlimitedDLL.pdb > nul || exit /b 1
 cd AAUnlimited\Texts || exit /b 1
 copy /Y *.lua %DST%\  > nul || exit /b 1
 xcopy /Y /E mod %DST%\mod > nul  || exit /b 1
