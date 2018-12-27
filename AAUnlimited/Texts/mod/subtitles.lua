b995e0f486f2b2e6b5727eeccf74346982484248
Genei Shouko <date.aa2@gmail.com>
2018-12-27 18:58:22 -0500

    subtitles lua backend

---
+++ AAUnlimited/Texts/mod/subtitles.lua
@@ -0,0 +1,44 @@
+--@INFO Shows subtitles
+
+local _M = {}
+local opts = {
+--	{ "maxlines", 4, "Maximum number of lines to show %i[1,10]"},
+--	{ "duration", 3, "Subtitles duration %i[1,10]:" },
+}
+
+local subtitles = {}
+
+local function reload_subtitles()
+	local dialogue
+	local count = 0
+	local subtitles_path = aau_path("subtitles.txt")
+	local file = io.open(subtitles_path, "r")
+	if not file then return end
+	for line in file:lines() do
+		file, dialogue = line:match("([%w_.]+).+\"(.*)\"$")
+		if file and dialogue then
+			subtitles[file] = dialogue
+			count = count + 1
+		end
+	end
+	log.info("Loaded %d subtitles", count)
+end
+
+function on.load_audio(fname)
+	local sub = subtitles[fname]
+	if sub then AddSubtitles(sub) end
+end
+
+function _M:load()
+	mod_load_config(self, opts)
+	reload_subtitles()
+end
+
+function _M:unload()
+end
+
+--function _M:config()
+--	mod_edit_config(self, opts, "Subtitles options")
+--end
+
+return _M
