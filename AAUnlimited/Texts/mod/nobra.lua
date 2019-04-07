da4cbcbbf6409643e3375626566e782a99b5deaf
Genei Shouko <date.aa2@gmail.com>
2019-04-07 13:10:03 -0500

    make no-bra configurable

--- AAUnlimited/Texts/mod/nobra.lua
+++ AAUnlimited/Texts/mod/nobra.lua
@@ -1,33 +1,38 @@
 --@INFO Overrides no bra physics status
 
 local _M = {}
---local opts = {
---	{ "maxlines", 4, "Maximum number of lines to show %i[1,10]"},
---	{ "duration", 3, "Subtitles duration %i[1,10]:" },
---}
+local opts = {
+	{ "allstates", "22 26 28 36 42 43 57 58 93 151 152 159 172 180 181 222 223 224 225 235", "Force no-bra in all states of: %s"},
+	{ "state1", "", "Force no-bra in state 1 of: %s" },
+	{ "state3", "155 156 188 230", "Force no-bra in state 3 of: %s" },
+}
 
 
 local function reload_overrides()
-	for slot in string.gmatch("22 26 28 36 42 43 57 58 93 151 152 159 172 180 181 222 223 224 225 235", "%d+") do
+	for slot in string.gmatch(opts.allstates, "%d+") do
 		SetNoBraOverride(tonumber(slot), 0xF)
 	end
-	for slot in string.gmatch("155 156 188 230", "%d+") do
+	for slot in string.gmatch(opts.state1, "%d+") do
+		SetNoBraOverride(tonumber(slot), 0x1)
+	end
+	for slot in string.gmatch(opts.state3, "%d+") do
 		SetNoBraOverride(tonumber(slot), 0x4)
 	end
 end
 
 
 function _M:load()
-	-- mod_load_config(self, opts)
+	mod_load_config(self, opts)
 	reload_overrides()
 end
 
 function _M:unload()
 end
 
---function _M:config()
---	mod_edit_config(self, opts, "Subtitles options")
---end
+function _M:config()
+	mod_edit_config(self, opts, "Subtitles options")
+	reload_overrides()
+end
 
 return _M
 
