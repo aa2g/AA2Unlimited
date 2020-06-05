157cc7d2c81b0799bcfe07731756aca62082061b
Genei Shouko <date.aa2@gmail.com>
2020-06-05 09:45:52 -0500

    update nobra default slots

--- AAUnlimited/Texts/mod/nobra.lua
+++ AAUnlimited/Texts/mod/nobra.lua
@@ -2,7 +2,7 @@
 
 local _M = {}
 
-local defallstates = "22 26 28 36 42 43 57 58 93 151 152 157 158 159 172 180 181 222 223 224 225 228 235"
+local defallstates = "22 26 28 36 42 43 57 58 93 151 152 157 158 159 166 167 172 180 181 222 223 224 225 228 235 252"
 local defstate1 = ""
 local defstate3 = "37 38 39 155 156 188 198 230 238 240 242"
 
@@ -19,11 +19,11 @@ local function reload_overrides()
 	for slot in string.gmatch(slots, "%d+") do
 		SetNoBraOverride(tonumber(slot), 0xF)
 	end
-	slots = opts.usedefaults == 1 and defstate1 or opts.states1
+	slots = opts.usedefaults == 1 and defstate1 or opts.state1
 	for slot in string.gmatch(slots, "%d+") do
 		SetNoBraOverride(tonumber(slot), 0x1)
 	end
-	slots = opts.usedefaults == 1 and defstate3 or opts.states3
+	slots = opts.usedefaults == 1 and defstate3 or opts.state3
 	for slot in string.gmatch(slots, "%d+") do
 		SetNoBraOverride(tonumber(slot), 0x4)
 	end
