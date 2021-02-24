ef6d5fec0df87619e61ccfca5af4c5dcd8f0645b
kayfour <githubakkcl@gmail.com>
2021-02-25 00:41:52 +0100

    Added an anti murder lua script, and clothing slots as they areread directly from the card

---
+++ AAUnlimited/Texts/mod/nomurder.lua
@@ -0,0 +1,25 @@
+--@INFO Disables murder actions
+
+local _M = {}
+local opts = {}
+local c = require 'const'
+
+function on.move(struct,user)
+	if (struct.conversationId == 81 or struct.conversationId == 82) then
+		struct.conversationId = -1
+		struct.target1 = 0
+	end
+end
+	
+function _M:load()
+	mod_load_config(self, opts)
+end
+
+function _M:unload()
+end
+
+function _M:config()
+	mod_edit_config(self, opts, "Jizou options")
+end
+
+return _M
